8e54b3548cf841c4f9f33b5605968a9d1e66daf1
matias lazarte <matiasnicolaslazarte@gmail.com>
2025-10-23 19:14:45 -0300

    feat: add Footer and Menu component

--- src/Components/shared/Menu.jsx
+++ src/Components/shared/Menu.jsx
@@ -1,9 +1,22 @@
+import { Navbar, Container, Nav, Button, NavLink } from "react-bootstrap";
+import { Link } from "react-router";
 
 const Menu = () => {
   return (
     <>
+       <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to={'/'} className="fs-2">Crud Food</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink to={'/'} className={"nav-link"}>HOME</NavLink>
+            <NavLink to={'/login'} className={'nav-link'}>LOGIN</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   )
 }
-
 export default Menu;
